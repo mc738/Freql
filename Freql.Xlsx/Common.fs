f554d2dcc5e252006a694bf54262d3f861a173b4
Max Clifford <me@mclifford.dev>
2023-06-21 21:22:58 +0100

    Freql.Xlsx.fsproj wip 1

---
+++ Freql.Xlsx/Common.fs
@@ -0,0 +1,32 @@
+﻿namespace Freql.Xlsx
+
+open DocumentFormat.OpenXml
+open DocumentFormat.OpenXml.Spreadsheet
+
+[<AutoOpen>]
+module Common =
+
+    open DocumentFormat.OpenXml.Packaging
+
+    let exec<'T> (fn: SpreadsheetDocument -> 'T) (isEditable: bool) (path: string) =
+        use doc = SpreadsheetDocument.Open(path, isEditable)
+
+        fn doc
+
+    let stringValue (str: string) = StringValue str
+
+    let getSheet (name: string) (doc: SpreadsheetDocument) =
+        doc.WorkbookPart.Workbook.Descendants<Sheet>()
+        |> Seq.tryFind (fun s -> s.Name = StringValue name)
+
+    let getSheetById (id: string) (doc: SpreadsheetDocument) =
+        doc.WorkbookPart.Workbook.Descendants<Sheet>()
+        |> Seq.tryFind (fun s -> s.Id = StringValue id)
+
+    let readCell (workSheet: WorksheetPart) (column: string) (row: int) =
+        workSheet.Worksheet.r
+
+        ()
+
+
+    ()
