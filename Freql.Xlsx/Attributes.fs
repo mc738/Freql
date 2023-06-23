79262704ce80d75409bf3a3b6f9fcb20e35cf915
Max Clifford <me@mclifford.dev>
2023-06-23 19:22:01 +0100

    Added Attributes.fs

---
+++ Freql.Xlsx/Attributes.fs
@@ -0,0 +1,14 @@
+﻿namespace Freql.Xlsx
+
+open System
+
+[<AutoOpen>]
+module Attributes =
+    
+    type XlsxColumnName(name: string) =
+
+        inherit Attribute()
+
+        member att.Name = name
+    
+
