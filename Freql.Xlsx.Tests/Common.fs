60413aa4fcd4e1240581895fa322f81d56b5750a
Max Clifford <me@mclifford.dev>
2023-06-24 11:09:17 +0100

    Added columnNameToIndex tests

---
+++ Freql.Xlsx.Tests/Common.fs
@@ -0,0 +1,70 @@
+﻿module Freql.Xlsx.Tests.Common
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Freql.Xlsx
+
+[<TestClass>]
+type TestClass () =
+
+    [<TestMethod>]
+    member this.``columnNameToIndex 'A' name`` () =
+        let columnName = "A"
+        
+        let expected = 0
+        
+        let actual = columnNameToIndex columnName 
+        
+        Assert.AreEqual(expected, actual)
+        
+    [<TestMethod>]
+    member this.``columnNameToIndex 'Z' name`` () =
+        let columnName = "Z"
+        
+        let expected = 25
+        
+        let actual = columnNameToIndex columnName 
+        
+        Assert.AreEqual(expected, actual)
+        
+    
+    [<TestMethod>]
+    member this.``columnNameToIndex 'AA' name`` () =
+        let columnName = "AA"
+        
+        let expected = 26
+        
+        let actual = columnNameToIndex columnName 
+        
+        Assert.AreEqual(expected, actual)
+        
+    
+    [<TestMethod>]
+    member this.``columnNameToIndex 'AZ' name`` () =
+        let columnName = "AZ"
+        
+        let expected = 51
+        
+        let actual = columnNameToIndex columnName 
+        
+        Assert.AreEqual(expected, actual)
+    
+    [<TestMethod>]
+    member this.``columnNameToIndex 'BA' name`` () =
+        let columnName = "BA"
+        
+        let expected = 52
+        
+        let actual = columnNameToIndex columnName 
+        
+        Assert.AreEqual(expected, actual)
+        
+    
+    [<TestMethod>]
+    member this.``columnNameToIndex 'BZ' name`` () =
+        let columnName = "BZ"
+        
+        let expected = 77
+        
+        let actual = columnNameToIndex columnName 
+        
+        Assert.AreEqual(expected, actual)
