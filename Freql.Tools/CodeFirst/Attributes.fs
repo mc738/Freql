0765b2f275786b7b53a3a7d25176e76b67402d0b
max <me@mclifford.dev>
2024-10-29 19:12:52 +0000

    Added Attributes.fs

---
+++ Freql.Tools/CodeFirst/Attributes.fs
@@ -0,0 +1,43 @@
+namespace Freql.Tools.CodeFirst
+
+open System
+open System.Reflection
+open Freql.Core.Utils.Attributes
+
+module Attributes =
+
+    [<AttributeUsage(AttributeTargets.Property)>]
+    type PrimaryKeyAttribute() =
+
+        inherit Attribute()
+
+    [<AttributeUsage(AttributeTargets.Property)>]
+    type ForeignKeyAttribute(otherType: Type) =
+
+        inherit Attribute()
+
+        member fka.OtherType = otherType
+
+    [<AttributeUsage(AttributeTargets.Property)>]
+    type IndexAttribute(unique: bool) =
+
+        inherit Attribute()
+        
+        member ia.Unique = unique
+
+    [<AttributeUsage(AttributeTargets.Property)>]
+    type IgnoreFieldAttribute() =
+        inherit Attribute()
+
+    
+    let getPrimaryKeyAttribute (propertyInfo: PropertyInfo) =
+        getAttributeFromProperty<PrimaryKeyAttribute> propertyInfo
+
+    let getForeignKeyAttribute (propertyInfo: PropertyInfo) =
+        getAttributeFromProperty<ForeignKeyAttribute> propertyInfo
+
+    let getIndexAttribute (propertyInfo: PropertyInfo) =
+        getAttributeFromProperty<IndexAttribute> propertyInfo
+
+    let getIgnoreAttribute (propertyInfo: PropertyInfo) =
+        getAttributeFromProperty<IgnoreFieldAttribute> propertyInfo
