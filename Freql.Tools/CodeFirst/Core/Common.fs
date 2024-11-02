a4376fd3e07f9388600d7af7275e98e252cd01a3
max <me@mclifford.dev>
2024-11-02 12:23:26 +0000

    Work on Common.fs

--- Freql.Tools/CodeFirst/Core/Common.fs
+++ Freql.Tools/CodeFirst/Core/Common.fs
@@ -1,11 +1,17 @@
 namespace Freql.Tools.CodeFirst.Core
 
+open System
+open System.Reflection
 open Freql.Core
 
 [<AutoOpen>]
 module Common =
 
-    type RecordInformation = { Name: string; Fields: FieldInformation list }
+    type RecordInformation =
+        { Name: string
+          Type: Type
+          Fields: FieldInformation list
+          VirtualFields: VirtualField list }
 
     and FieldInformation =
         { Name: string
@@ -13,26 +19,64 @@ module Common =
           PrimaryKey: PrimaryKeyDefinitionType option
           ForeignKey: FieldForeignKey option
           Index: FieldIndex option
-          /// <summary>
-          /// A virtual field is one that doesn't exist on the record but is used for generation.
-          /// For example if type `Foo` has a field of type `Bar`, `Bar` will have a virtual field added of `FooId`
-          /// which will be marked as a foreign key.
-          /// </summary>
-          VirtualField: bool }
+          PropertyInformation: PropertyInfo
+          Optional: bool }
 
     and [<RequireQualifiedAccess>] PrimaryKeyDefinitionType =
         | Attribute
         | Convention
-    
+
     and FieldForeignKey =
         { TypeName: string
+          Type: Type
           FieldName: string option }
+
+        member ffk.GetTypeName() = ffk.Type.Name
+
+        member ffk.GetTypeFullName() = ffk.Type.FullName
 
     and FieldIndex = { Unique: bool }
 
     and FieldType =
         | SupportedType of SupportedType
-        | Child of RecordInformation
+        | Record of Type
+        | Collection of FSharpCollectionType
 
+    and VirtualField =
+        { Type: Type }
 
-    ()
+        member vf.GetTypeName() = vf.Type.Name
+
+        member vf.GetTypeFullName() = vf.Type.FullName
+
+    [<RequireQualifiedAccess>]
+    type ScalarValue =
+        | Boolean of bool
+        | Byte of byte
+        | SByte of int8
+        | Char of char
+        | Decimal of decimal
+        | Double of double
+        | Single of single
+        | Int16 of int16
+        | UInt16 of uint16
+        | Int of int
+        | UInt of uint32
+        | Int64 of int64
+        | UInt64 of uint64
+        | String of string
+        | DateTime of DateTime
+        | Guid of Guid
+
+    [<RequireQualifiedAccess>]
+    type PrimaryKeyType =
+        | Simple
+        | Composite
+
+    type PrimaryKeyValue =
+        | Simple of PrimaryKeyValuePart
+        | Composite of PrimaryKeyValuePart list
+
+    and PrimaryKeyValuePart =
+        { FieldName: string
+          Value: ScalarValue }
