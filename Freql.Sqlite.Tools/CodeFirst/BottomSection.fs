b3265cc52d930b93fd5d35c2da7990e24dc3d96c
max <me@mclifford.dev>
2024-11-03 22:04:54 +0000

    Added BottomSection.fs

---
+++ Freql.Sqlite.Tools/CodeFirst/BottomSection.fs
@@ -0,0 +1,8 @@
+namespace Freql.Sqlite.Tools.CodeFirst
+
+[<RequireQualifiedAccess>]
+module BottomSection =
+
+    open Freql.Tools.CodeFirst.CodeGeneration
+
+    let generate (ctx: CodeGeneratorContext) = []
