b944735b3208de30c3c6097aa5ef9eab9e51ce57
max <me@mclifford.dev>
2024-11-04 09:51:19 +0000

    Work on Boilerplate.fs

--- Freql.Tools/CodeFirst/CodeGeneration/Boilerplate.fs
+++ Freql.Tools/CodeFirst/CodeGeneration/Boilerplate.fs
@@ -1,18 +1,20 @@
 namespace Freql.Tools.CodeFirst.CodeGeneration
 
 module Boilerplate =
-    
+
     open Freql.Tools.CodeGeneration.Boilerplate
-    
+
     let fileHeader (ctx: CodeGeneratorContext) (fileNamespace: string) (openFiles: string list) =
-         [ yield! Header.lines
-           $"namespace {fileNamespace}"
-           ""
-           "#nowarn \"6142001\""
-           ""
-           "open System"
-           "open Freql.Tools.CodeFirst"
-           "open Freql.Tools.CodeFirst.Core.Operations"
-           yield! openFiles |> List.map (fun openFile -> $"open {openFile}") ]
-
-
+        [ yield! Header.lines
+          $"namespace {fileNamespace}"
+          ""
+          "#nowarn \"6142001\""
+          ""
+          "open System"
+          "open Freql.Tools.CodeFirst"
+          "open Freql.Tools.CodeFirst.Core"
+          "open Freql.Tools.CodeFirst.Core.Operations"
+          yield!
+              ctx.DatabaseSpecificProfile.OpenReferences
+              |> List.map (fun openReference -> $"open {openReference}")
+          yield! openFiles |> List.map (fun openFile -> $"open {openFile}") ]
