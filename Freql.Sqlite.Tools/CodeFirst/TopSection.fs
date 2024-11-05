97aebbd9375b821d8ad0e7f2369987896e63b6a4
max <me@mclifford.dev>
2024-11-05 19:03:15 +0000

    Added TopSection.fs

---
+++ Freql.Sqlite.Tools/CodeFirst/TopSection.fs
@@ -0,0 +1,8 @@
+namespace Freql.Sqlite.Tools.CodeFirst
+
+[<RequireQualifiedAccess>]
+module TopSection =
+
+    open Freql.Tools.CodeFirst.CodeGeneration
+
+    let generate (ctx: CodeGeneratorContext) = []
