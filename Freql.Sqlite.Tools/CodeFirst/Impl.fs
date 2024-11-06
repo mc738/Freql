175aef80ed8d01d0c3e8656f6098fa617ad08292
max <me@mclifford.dev>
2024-11-06 20:18:43 +0000

    Work on Impl.fs

--- Freql.Sqlite.Tools/CodeFirst/Impl.fs
+++ Freql.Sqlite.Tools/CodeFirst/Impl.fs
@@ -18,6 +18,10 @@ module Impl =
                    TopSection = TopSection.generate
                    TypeExtension = TypeExtensions.generate
                    OperationGenerator = OperationsGenerator.generate
+                   CreateGenerator = OperationsGenerator.Create.generate
+                   ReadGenerator = OperationsGenerator.Read.generate
+                   UpdateGenerator = OperationsGenerator.Update.generate
+                   DeleteGenerator = OperationsGenerator.Delete.generate
                    BottomSection = BottomSection.generate } }
             : CodeGeneratorContext)
         | Error errorValue -> failwith "todo"
