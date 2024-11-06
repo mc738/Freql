5acb65ddb3c8e31a17caf34e9accafd7a702e05d
max <me@mclifford.dev>
2024-11-06 20:15:06 +0000

    Work on TypeExtensions.fs

--- Freql.Sqlite.Tools/CodeFirst/TypeExtensions.fs
+++ Freql.Sqlite.Tools/CodeFirst/TypeExtensions.fs
@@ -14,7 +14,7 @@ module TypeExtensions =
     let generateSelectSql
         (ctx: CodeGeneratorContext)
         (record: RecordInformation)
-        (filteredFields: (FieldInformation * SupportedType) list)
+        (scalarFields: ScalarField list)
         =
         let normalizedName =
             match ctx.NameNormalizationMode with
@@ -27,15 +27,15 @@ module TypeExtensions =
           "\"\"\"" |> indent1
           $"CREATE TABLE IF NOT EXISTS {normalizedName} (" |> indent1
           yield!
-              filteredFields
-              |> List.mapi (fun i (field, supportedType) ->
+              scalarFields
+              |> List.mapi (fun i scalarField ->
                   let normalizedFieldName =
                       match ctx.NameNormalizationMode with
-                      | PascalCase -> field.Name.ToPascalCase()
-                      | SnakeCase -> field.Name.ToSnakeCase()
+                      | PascalCase -> scalarField.Field.Name.ToPascalCase()
+                      | SnakeCase -> scalarField.Field.Name.ToSnakeCase()
 
                   let primaryKey =
-                      match field.PrimaryKey with
+                      match scalarField.Field.PrimaryKey with
                       | None -> ""
                       | Some PrimaryKeyDefinitionType.Attribute -> " PRIMARY KEY"
                       | Some(PrimaryKeyDefinitionType.Convention priority) ->
@@ -47,19 +47,19 @@ module TypeExtensions =
                           | false, false -> ""
 
                   let eol =
-                      match i = filteredFields.Length - 1 with
+                      match i = scalarFields.Length - 1 with
                       | true -> ""
                       | false -> ","
 
-                  $"{normalizedFieldName} {supportedTypeToSqliteType supportedType}{primaryKey}{eol}")
+                  $"{normalizedFieldName} {supportedTypeToSqliteType scalarField.SupportedType}{primaryKey}{eol}")
               |> List.map (indent 2)
           ")" |> indent1
-          "\"\"\"" ]
+          "\"\"\"" |> indent1 ]
 
     let generateInsertSql
         (ctx: CodeGeneratorContext)
         (record: RecordInformation)
-        (filteredFields: (FieldInformation * SupportedType) list)
+        (scalarFields: ScalarField list)
         =
         let normalizedName =
             match ctx.NameNormalizationMode with
@@ -67,15 +67,15 @@ module TypeExtensions =
             | SnakeCase -> record.Name.ToSnakeCase()
 
         let (fields, parameters) =
-            filteredFields
-            |> List.mapi (fun i (field, supportedType) ->
+            scalarFields
+            |> List.mapi (fun i scalarField ->
                 let normalizedFieldName =
                     match ctx.NameNormalizationMode with
-                    | PascalCase -> field.Name.ToPascalCase()
-                    | SnakeCase -> field.Name.ToSnakeCase()
+                    | PascalCase -> scalarField.Field.Name.ToPascalCase()
+                    | SnakeCase -> scalarField.Field.Name.ToSnakeCase()
 
                 let eol =
-                    match i = filteredFields.Length - 1 with
+                    match i = scalarFields.Length - 1 with
                     | true -> ""
                     | false -> ", "
 
@@ -88,24 +88,109 @@ module TypeExtensions =
           "\"\"\"" |> indent1
           $"INSERT INTO {normalizedName} ({fields})" |> indent1
           $"VALUES ({parameters})" |> indent1
-          "\"\"\"" ]
+          "\"\"\"" |> indent1 ]
+
+    let generateUpdateSql
+        (ctx: CodeGeneratorContext)
+        (record: RecordInformation)
+        (scalarFields: ScalarField list)
+        =
+        let normalizedName =
+            match ctx.NameNormalizationMode with
+            | PascalCase -> record.Name.ToPascalCase()
+            | SnakeCase -> record.Name.ToSnakeCase()
+
+        let parameters =
+            scalarFields
+            |> List.mapi (fun i scalarField ->
+                let normalizedFieldName =
+                    match ctx.NameNormalizationMode with
+                    | PascalCase -> scalarField.Field.Name.ToPascalCase()
+                    | SnakeCase -> scalarField.Field.Name.ToSnakeCase()
+
+                $"{normalizedFieldName} = @{i}")
+            |> String.concat ", "
+
+        let whereClause =
+            match record.GetActualPrimaryKeyFields() with
+            | [] -> failwith "No primary key"
+            | [ pk ] ->
+                let normalizedFieldName =
+                    match ctx.NameNormalizationMode with
+                    | PascalCase -> pk.Name.ToPascalCase()
+                    | SnakeCase -> pk.Name.ToSnakeCase()
+
+                $"{normalizedFieldName} = @{scalarFields.Length}"
+            | pks ->
+                let clause =
+                    pks
+                    |> List.mapi (fun i pk ->
+                        let normalizedFieldName =
+                            match ctx.NameNormalizationMode with
+                            | PascalCase -> pk.Name.ToPascalCase()
+                            | SnakeCase -> pk.Name.ToSnakeCase()
+
+                        $"{normalizedFieldName} = @{scalarFields.Length + i}")
+                    |> String.concat ", "
+
+                $"({clause})"
+
+        [ "member this.UpdateRecordsSql(updates: UpdateFieldOperation list) ="
+          "\"\"\"" |> indent1
+          $"UPDATE {normalizedName}" |> indent1
+          $"SET {parameters}" |> indent1
+          $"WHERE {whereClause}" |> indent1
+          "\"\"\"" |> indent1 ]
+        
+    let generateDeleteSql
+        (ctx: CodeGeneratorContext)
+        (record: RecordInformation)
+        (scalarFields: ScalarField list)
+        =
+        let normalizedName =
+            match ctx.NameNormalizationMode with
+            | PascalCase -> record.Name.ToPascalCase()
+            | SnakeCase -> record.Name.ToSnakeCase()
+
+        let whereClause =
+            match record.GetActualPrimaryKeyFields() with
+            | [] -> failwith "No primary key"
+            | [ pk ] ->
+                let normalizedFieldName =
+                    match ctx.NameNormalizationMode with
+                    | PascalCase -> pk.Name.ToPascalCase()
+                    | SnakeCase -> pk.Name.ToSnakeCase()
+
+                $"{normalizedFieldName} = @0"
+            | pks ->
+                let clause =
+                    pks
+                    |> List.mapi (fun i pk ->
+                        let normalizedFieldName =
+                            match ctx.NameNormalizationMode with
+                            | PascalCase -> pk.Name.ToPascalCase()
+                            | SnakeCase -> pk.Name.ToSnakeCase()
+
+                        $"{normalizedFieldName} = @{i}")
+                    |> String.concat ", "
+
+                $"({clause})"
+
+        [ "member this.DeleteRecordsSql() ="
+          "\"\"\"" |> indent1
+          $"DELETE {normalizedName}" |> indent1
+          $"WHERE {whereClause}" |> indent1
+          "\"\"\"" |> indent1 ]
 
     let generate (ctx: CodeGeneratorContext) (record: RecordInformation) =
-        let filteredFields =
-            record.Fields
-            |> List.choose (fun field ->
-                match field.Type with
-                | SupportedType supportedType -> Some(field, supportedType)
-                | Record ``type`` ->
-                    // Do nothing, this will be a foreign key in field ``type``
-                    None
-                | Collection collectionType ->
-                    // Do nothing, this will be a foreign key in field ``type``
-                    None)
+        let scalarFields = record.GetScalarFields()
 
-
-        [ yield! generateSelectSql ctx record filteredFields
+        [ yield! generateSelectSql ctx record scalarFields
           ""
-          yield! generateInsertSql ctx record filteredFields
+          yield! generateInsertSql ctx record scalarFields
+          ""
+          yield! generateUpdateSql ctx record scalarFields
+          ""
+          yield! generateDeleteSql ctx record scalarFields
 
           ]
