ffa145c22b9cc9c9dbc65653dcf1ad34019a7504
max <me@mclifford.dev>
2024-11-07 19:28:25 +0000

    Work on Operations.fs

--- Freql.Sqlite.Tools/CodeFirst/Operations.fs
+++ Freql.Sqlite.Tools/CodeFirst/Operations.fs
@@ -25,7 +25,24 @@ module OperationsGenerator =
     [<RequireQualifiedAccess>]
     module Update =
 
-        let generate (ctx: CodeGeneratorContext) (record: RecordInformation) = []
+        let generate (ctx: CodeGeneratorContext) (record: RecordInformation) = [
+            "let updates ="
+            "ops" |> indent1
+            "|> List.choose (function" |> indent1
+            "| RecordTrackingOperation.UpdateField update -> Some update" |> indent 2
+            "| _ -> None)" |> indent 2
+            ""
+            "match"
+            "ctx.TryExecuteVerbatimNonQueryAnon(" |> indent1
+            "newRecord.UpdateRecordsSql(updates)," |> indent 2
+            "updates |> List.map (fun update -> update.NewValue)" |> indent 2
+            ")" |> indent1
+            "with"
+            "| Ok _ ->"
+            "// TODO update primary key if required" |> indent1
+            "Ok newRecord" |> indent1
+            "| Error failure -> failure.GetMessage() |> Error"
+        ]
 
     [<RequireQualifiedAccess>]
     module Delete =
