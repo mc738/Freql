6b5040efc89f4d7b2aac90b5cac2036d028e5f43
max <me@mclifford.dev>
2025-01-16 20:45:02 +0000

    Work on Common.fs

--- Freql.Tools/CodeFirst/Core/Common.fs
+++ Freql.Tools/CodeFirst/Core/Common.fs
@@ -1,6 +1,7 @@
 namespace Freql.Tools.CodeFirst.Core
 
 open System
+open System.Collections.Generic
 open System.Reflection
 open Freql.Core
 
@@ -13,6 +14,86 @@ module Common =
           Fields: FieldInformation list
           VirtualFields: VirtualField list }
 
+        member ri.HasDefinedPrimaryKey() =
+            ri.Fields |> List.exists (fun f -> f.PrimaryKey.IsSome)
+
+        member ri.HasAttributePrimaryKey() =
+            ri.Fields
+            |> List.exists (fun f ->
+                match f.PrimaryKey with
+                | Some(PrimaryKeyDefinitionType.Attribute) -> true
+                | _ -> false)
+
+        member ri.HasConventionPrimaryKey() =
+            ri.Fields
+            |> List.exists (fun f ->
+                match f.PrimaryKey with
+                | Some(PrimaryKeyDefinitionType.Convention _) -> true
+                | _ -> false)
+
+        member ri.GetAllPrimaryKeyFields() =
+            ri.Fields |> List.filter (fun f -> f.PrimaryKey.IsSome)
+
+        member ri.GetAttributePrimaryKeyFields() =
+            ri.Fields
+            |> List.filter (fun f ->
+                match f.PrimaryKey with
+                | Some(PrimaryKeyDefinitionType.Attribute) -> true
+                | _ -> false)
+
+        member ri.GetConventionPrimaryKeyFields() =
+            ri.Fields
+            |> List.filter (fun f ->
+                match f.PrimaryKey with
+                | Some(PrimaryKeyDefinitionType.Convention _) -> true
+                | _ -> false)
+
+        member ri.GetTopConventionPrimaryKeyField() =
+            ri.Fields
+            |> List.fold
+                (fun top curr ->
+                    match curr.PrimaryKey with
+                    | Some(PrimaryKeyDefinitionType.Convention priority) when priority > top -> priority
+                    | _ -> top)
+                -1
+
+        /// <summary>
+        /// Get the actual primary key field(s).
+        /// This means attribute ones will be chosen first,
+        /// if not the convention based one with the highest priority will be chosen.
+        /// </summary>
+        member ri.GetActualPrimaryKeyFields() =
+            match ri.HasAttributePrimaryKey(), ri.HasConventionPrimaryKey() with
+            | true, _ -> ri.GetAttributePrimaryKeyFields()
+            | _, true ->
+                ri.GetConventionPrimaryKeyFields()
+                |> List.sortByDescending (fun field ->
+                    match field.PrimaryKey with
+                    | Some(PrimaryKeyDefinitionType.Convention v) -> v
+                    | _ -> -1)
+            | _, _ -> []
+
+        member ri.GetScalarFields() =
+            ri.Fields
+            |> List.choose (fun field ->
+                match field.Type with
+                | SupportedType supportedType -> { Field = field; SupportedType = supportedType } |> Some
+                | Record ``type`` ->
+                    // Do nothing, this will be a foreign key in field ``type``
+                    None
+                | Collection collectionType ->
+                    // Do nothing, this will be a foreign key in field ``type``
+                    None)
+        
+        member ri.GetPrimaryKeyScalarFields() =
+            ri.Fields
+            |> List.choose (fun field ->
+                match field.PrimaryKey.IsSome, field.Type with
+                | true, SupportedType supportedType -> { Field = field; SupportedType = supportedType } |> Some
+                | false, _
+                | _, Record _
+                | _, Collection _ -> None)
+        
     and FieldInformation =
         { Name: string
           Type: FieldType
@@ -22,9 +103,14 @@ module Common =
           PropertyInformation: PropertyInfo
           Optional: bool }
 
+    and ScalarField =
+        { Field: FieldInformation
+          SupportedType: SupportedType }
+
+
     and [<RequireQualifiedAccess>] PrimaryKeyDefinitionType =
         | Attribute
-        | Convention
+        | Convention of Priority: int
 
     and FieldForeignKey =
         { TypeName: string
@@ -43,11 +129,20 @@ module Common =
         | Collection of FSharpCollectionType
 
     and VirtualField =
-        { Type: Type }
+        | PrimaryKey of VirtualPrimaryKeyField
+        | ForeignKey of VirtualForeignKeyField
 
-        member vf.GetTypeName() = vf.Type.Name
+    and VirtualPrimaryKeyField = { Type: Type }
 
-        member vf.GetTypeFullName() = vf.Type.FullName
+    and VirtualForeignKeyField =
+        { Type: Type
+          ValueType: PrimaryKeyType }
+
+    and [<RequireQualifiedAccess>] PrimaryKeyType =
+        | Simple
+        | Composite
+        | Virtual
+
 
     [<RequireQualifiedAccess>]
     type ScalarValue =
@@ -66,16 +161,14 @@ module Common =
         | UInt64 of uint64
         | String of string
         | DateTime of DateTime
+        | TimeSpan of TimeSpan
         | Guid of Guid
 
     [<RequireQualifiedAccess>]
-    type PrimaryKeyType =
-        | Simple
-        | Composite
-
     type PrimaryKeyValue =
         | Simple of PrimaryKeyValuePart
         | Composite of PrimaryKeyValuePart list
+        | Virtual of PrimaryKeyValuePart
 
     and PrimaryKeyValuePart =
         { FieldName: string
